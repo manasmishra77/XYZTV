a4fc107559d5cd96d1e56d8fa08645a8b176f76c
Pallav Trivedi <pallav.trivedi@ril.com>
2017-09-05 12:33:37 +0530

    Cell size reduced

--- JioCinema/JCPlayerVC.swift
+++ JioCinema/JCPlayerVC.swift
@@ -58,10 +58,6 @@ class JCPlayerVC: UIViewController
         removePlayerObserver()
     }
     
-//    override func viewDidDisappear(_ animated: Bool) {
-//        //self.removePlayerObserver()
-//    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
