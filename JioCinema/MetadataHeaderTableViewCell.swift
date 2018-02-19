87efda3fffec4847f0449119b2cd6dd482d4f3db
manasmishra <manas1.mishra@ril.com>
2018-02-19 20:05:05 +0530

    HeaderViewWithCell

---
+++ JioCinema/MetadataHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MetadataHeaderTableViewCell.swift
+//  JioCinema
+//
+//  Created by manas on 19/02/18.
+//  Copyright © 2018 Reliance Jio Infocomm. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MetadataHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
