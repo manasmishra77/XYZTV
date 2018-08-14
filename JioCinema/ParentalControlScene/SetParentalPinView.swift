b2e72028cc52014d57c543dc26ff2429637b8d7e
Vinit Somani <vinit.somani@ril.com>
2018-08-14 12:52:07 +0530

    missing file SetParentalPinView added.

---
+++ JioCinema/ParentalControlScene/SetParentalPinView.swift
@@ -0,0 +1,15 @@
+//
+//  SetPINViewController.swift
+//  JioCinema
+//
+//  Created by Shweta Adagale on 13/08/18.
+//  Copyright © 2018 Reliance Jio Infocomm. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SetParentalPinView: UIView {
+
+    @IBOutlet weak var pinLabel: UILabel!
+
+}
