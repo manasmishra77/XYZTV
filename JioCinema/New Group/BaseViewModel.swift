84749b90b36894d0b761db2336be125da9872e3e
manas <manas1.mishra@ril.com>
2018-10-19 12:16:13 +0530

    Pagination SetUp

--- JioCinema/New Group/BaseViewModel.swift
+++ JioCinema/New Group/BaseViewModel.swift
@@ -100,7 +100,7 @@ class BaseViewModel: NSObject  {
     var errorMsg: String?
     
     var totalPage: Int {// Reference for Downloading base page
-        return baseDataModel?.totalPages ?? 0
+        return baseDataModel?.totalPages ?? 1
     }
     var viewResponseBlock: ((_ isSuccess: Bool) -> ())? = nil
     lazy var baseAPIReponseHandler = {(_ isSuccess: Bool, error: String?) in
@@ -138,6 +138,7 @@ class BaseViewModel: NSObject  {
          RJILApiManager.getWatchListData(isDisney : true ,type: .disneyTVShow, nil)
     }
     func fetchBaseData() {
+        guard pageNumber < totalPage else {return}
         RJILApiManager.getBaseModel(pageNum: pageNumber, type: vcType) {[unowned self] (isSuccess, errMsg) in
             guard isSuccess else {
                 self.errorMsg = errMsg
@@ -161,8 +162,8 @@ class BaseViewModel: NSObject  {
         case .base:
             if let dataContainer = baseDataModel?.data {
                 let data = dataContainer[(itemIndexTuple.1)]
-                if itemIndexTuple.1 == dataContainer.count - 2 {
-                    // fetchHomeData()
+                if itemIndexTuple.1 == dataContainer.count - 1 {
+                    fetchBaseData()
                 }
                 return (title: data.title ?? "", items: data.items ?? [], cellType: .base)
             }
