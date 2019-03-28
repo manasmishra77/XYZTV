35dc122c8d566b7ffc177a1671018f6d72231e15
Vinit Somani <vinit.somani@ril.com>
2019-03-28 18:03:59 +0530

    WIP player

--- JioCinema/ApiManager/RJILApiManager.swift
+++ JioCinema/ApiManager/RJILApiManager.swift
@@ -71,10 +71,10 @@ class RJILApiManager {
             _commonHeaders["x-multilang"] = "true"
             _commonHeaders["X-API-Key"] = apIKey
             _commonHeaders["app-name"] = "RJIL_JioCinema"
+            _commonHeaders["x-apisignatures"] = "5772987301"
             
             if JCLoginManager.sharedInstance.isUserLoggedIn() {
                 _commonHeaders["uniqueid"] = JCAppUser.shared.unique
-                _commonHeaders["uniqueId"] = JCAppUser.shared.unique
                 _commonHeaders["ua"] = "(\(UIDevice.current.model) ; OS \(UIDevice.current.systemVersion) )"
                 _commonHeaders["accesstoken"] = JCAppUser.shared.ssoToken
                 _commonHeaders["ssotoken"] = JCAppUser.shared.ssoToken
