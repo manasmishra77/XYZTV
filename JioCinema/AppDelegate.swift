31ae570af219a95fd35e7e5aef509603ba916fff
Shweta Adagale <Shweta.Adagale@ril.com>
2019-07-30 17:42:49 +0530

    Url decoding

--- JioCinema/AppDelegate.swift
+++ JioCinema/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
+        func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
@@ -69,6 +69,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         
@@ -86,6 +87,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if urlString.contains("jCApp:?identifier="){
             if let urlSubString = urlString.dropFirst(18).removingPercentEncoding{
                 //topShelfContentModel = VODTopShelfModel.getModel(urlSubString)
+            }
+        }
+        if urlString.contains("jCApp:?contentId="){
+            var tappeditem = Item()
+            if let url = URL(string: urlString){
+            let urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: true)
+                
+            //Don't change the sequence (Very confidential)
+            let queryItems = urlComponent?.queryItems
+                tappeditem.id = queryItems?[0].value
+                tappeditem.latestId = queryItems?[1].value
+                tappeditem.isPlaylist = Bool(queryItems?[2].value ?? "false")
+                tappeditem.playlistId = queryItems?[3].value
+                var appType = App()
+                appType.type = Int(queryItems?[4].value ?? "0")
+                tappeditem.app = appType
+
+                
             }
         }
         return false
