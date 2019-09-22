f36e03e8b71045231ce9cf76748ec66c487873f3
Shweta Adagale <Shweta.Adagale@ril.com>
2019-09-22 19:46:43 +0530

    constraints added

--- JioCinema/New Group/TvPosterCollectionViewCell.swift
+++ JioCinema/New Group/TvPosterCollectionViewCell.swift
@@ -18,67 +18,92 @@ class ItemCell: UICollectionViewCell {
 @available(tvOS 12.1, *)
 
 class TvPosterCollectionViewCell: ItemCell {
+    //@IBOutlet weak var posterView: TVPosterView!
     var posterView: TVPosterView?
     var monogramView: TVMonogramView?
+    var  cellItems: BaseItemCellModel?
+    var layout: ItemCellLayoutType = .landscapeWithLabels
     
-//    @IBOutlet weak var poserView: TVPosterView!
     func configureCell(_ cellItems: BaseItemCellModel, isPlayingNow: Bool = false) {
+        layout = cellItems.layoutType ?? .landscapeWithLabels
+        self.cellItems = cellItems
         setLayoutOfCell()
-        setValuesToCell(cellItems)
+        setValuesToCell()
         
     }
-    override func prepareForReuse() {
-        posterView?.removeFromSuperview()
-        posterView?.image = nil
-        posterView = nil
-    }
+    
+    override func awakeFromNib() {
+        posterView = TVPosterView(frame: self.bounds)
+        posterView?.contentSize = CGSize(width: self.bounds.width - 120, height: self.bounds.height - 120)
+
+        posterView?.focusSizeIncrease = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing:10)
+        posterView?.addAsSubViewWithConstraints(self)
+     }
     
     func setLayoutOfCell(){
+        
     }
     
-    func setValuesToCell(_ cellItems: BaseItemCellModel){
-        posterView = TVPosterView(frame: self.bounds)
-        setImageForPosterView(cellItem: cellItems)
-//        if let imageURL = URL(string: cellItems.item?.imageUrlLandscapContent ?? "") {
-//
-//            SDWebImageManager.shared.loadImage(with: imageURL, options: .continueInBackground, progress: nil) { (image: UIImage?, data: Data?, error: Error?, sdImageCatcheType: SDImageCacheType, finished: Bool, imageUrl: URL?) in
-//                self.posterView?.image = image
-//                self.posterView?.contentMode = .scaleAspectFit
+    func setValuesToCell(){
+        setTitleSubtitleOfCell()
+        setImageForPosterView()
+    }
+    func setTitleSubtitleOfCell(isFocused: Bool = false) {
+//        switch layout {
+//        case .potrait:
+//            posterView?.subtitle = ""
+//            if isFocused {
+//                posterView?.title = cellItems?.item?.name ?? ""
+//            } else {
+//                posterView?.title = ""
 //            }
-//
+//        case .landscapeWithTitleOnly:
+//            posterView?.subtitle = ""
+//            if isFocused {
+//                posterView?.title = cellItems?.item?.name ?? ""
+//            } else {
+//                posterView?.title = ""
+//            }
+//        case .landscapeForResume:
+//            break
+//        case .landscapeForLangGenre:
+//            posterView?.title = ""
+//            posterView?.subtitle = ""
+//            break
+//        case .landscapeWithLabels:
+//            if isFocused {
+//                posterView?.title = cellItems?.item?.name ?? ""
+//                posterView?.subtitle = cellItems?.item?.subtitle ?? ""
+//            } else {
+//                posterView?.title = ""
+//                posterView?.subtitle = ""
+//            }
+//            break
+//        case .landscapeWithLabelsAlwaysShow:
+//            break
+//        case .disneyCharacter:
+//            posterView?.title = ""
+//            posterView?.subtitle = ""
+//            break
 //        }
-        posterView?.frame = self.bounds
-        
-        
-        
-        
-        posterView?.title = cellItems.item?.name
-        posterView?.subtitle = cellItems.item?.subtitle
-    
-//        posterView?.
-        posterView?.imageView.contentMode = .scaleAspectFit
-        posterView?.contentSize = self.bounds.size
-        posterView?.footerView?.backgroundColor = .red
-        posterView?.headerView?.backgroundColor = .yellow
-        posterView?.contentView.backgroundColor = .green
-        var stringAttribute : String.StringAttribute?
-        stringAttribute?.fontSize = 10
-//        posterView?.subtitle = posterView?.subtitle?.getFontifiedText(partOfTheStringNeedToConvert: [stringAttribute!])
-        posterView?.backgroundColor = .blue
-        self.addSubview(self.posterView!)
+        posterView?.title = cellItems?.item?.name
+        if let subtitle = cellItems?.item?.subtitle , subtitle != ""{
+            posterView?.subtitle = subtitle
+        } else {
+            posterView?.subtitle = "asnf"
+        }
     }
-    func setImageForPosterView(cellItem: BaseItemCellModel) {
+    func setImageForPosterView() {
         var imageUrl = ""
-        if let charItem = cellItem.charactorItems{
+        if let charItem = cellItems?.charactorItems {
             imageUrl = charItem.LogoUrlForDisneyChar
-        } else if let item = cellItem.item {
+        } else if let item = cellItems?.item {
             imageUrl = item.imageUrlLandscapContent
         }
         if let imageURL = URL(string: imageUrl) {
         
             SDWebImageManager.shared.loadImage(with: imageURL, options: .continueInBackground, progress: nil) { (image: UIImage?, data: Data?, error: Error?, sdImageCatcheType: SDImageCacheType, finished: Bool, imageUrl: URL?) in
                 self.posterView?.image = image
-                self.posterView?.contentMode = .scaleAspectFit
             }
             
         }
